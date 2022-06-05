8613bd7078610e18b429a1750f71b66c65ee6fa4
pmmm3 <pablo.triplem@gmail.com>
2022-06-05 19:14:26 +0200

    MS - Landing Page

--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,51 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node'
 import {
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+  ScrollRestoration
+} from '@remix-run/react'
+
+import globalStyles from '~/styles/global.css'
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+  charset: 'utf-8',
+  title: 'New Remix App',
+  viewport: 'width=device-width,initial-scale=1'
+})
+export const links = () => ([
+  {
+    rel: 'stylesheet',
+    href: globalStyles
+  }
+])
 
-export default function App() {
+function Layout () {
   return (
-    <html lang="en">
+    <>
+        <header></header>
+        <Outlet />
+        <footer> &copy; Copyright 2022 PabloMoreno.</footer>
+    </>
+  )
+}
+
+export default function App () {
+  return (
+    <html lang="es">
       <head>
+        <title>Pablo Moreno</title>
         <Meta />
         <Links />
       </head>
       <body>
-        <Outlet />
+        <Layout />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
