1908e4eff73690a8f39fbcf7096d4430959b8d2e
pmmm3 <pablo.triplem@gmail.com>
2022-06-06 01:37:53 +0200

    Add 404 pagee

--- app/root.tsx
+++ app/root.tsx
@@ -1,40 +1,60 @@
-import type { MetaFunction } from '@remix-run/node'
+import type { MetaFunction } from '@remix-run/node';
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
+  NavLink,
   Outlet,
   Scripts,
-  ScrollRestoration
-} from '@remix-run/react'
+  ScrollRestoration,
+  useCatch,
+} from '@remix-run/react';
 
-import globalStyles from '~/styles/global.css'
+import globalStyles from '~/styles/global.css';
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
   title: 'New Remix App',
   viewport: 'width=device-width,initial-scale=1'
-})
-export const links = () => ([
+});
+export const links = () => [
   {
     rel: 'stylesheet',
     href: globalStyles
+  },
+  {
+    rel: 'stylesheet',
+    href: 'https://cdn.simplecss.org/simple.css'
   }
-])
-
+];
 function Layout () {
   return (
     <>
-        <header></header>
-        <Outlet />
-        <footer> &copy; Copyright 2022 PabloMoreno.</footer>
+      <header>
+        <h4>Pablo Moreno</h4>
+        <nav>
+          <NavLink to='' end>
+            Inicio
+          </NavLink>
+          <NavLink to='/works'>Trabajos</NavLink>
+          <NavLink to='/skills'>Skills</NavLink>
+          <a href='~/pdfs/sample.pdf' download>
+            Curriculum en PDF
+          </a>
+        </nav>
+      </header>
+      <Outlet />
+      <footer>
+        <small> &copy; Copyright 2022 PabloMoreno.</small>
+      </footer>
     </>
-  )
+  );
 }
 
-export default function App () {
+export default function App() {
   return (
-    <html lang="es">
+    <html lang='es'>
       <head>
         <title>Pablo Moreno</title>
         <Meta />
@@ -47,5 +67,27 @@ export default function App () {
         <LiveReload />
       </body>
     </html>
-  )
+  );
+}
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  return (
+    <html>
+      <head>
+        <title>Oops!</title>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <h1>{caught.status + ' ' + caught.statusText + '😬'}</h1>
+        <img
+          alt={caught.status + ' ' + caught.statusText}
+          width='100%'
+          src='assets/404/16.png'
+        />
+        <Scripts />
+      </body>
+    </html>
+  );
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,5 @@
 export default function Index () {
   return (
-    <h1>Pablo Moreno</h1>
+    <h1>Inicio</h1>
   )
 }
